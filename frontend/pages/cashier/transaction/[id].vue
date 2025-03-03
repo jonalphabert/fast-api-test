cd2cfe33d855212e862d811cb8eb81f3dfeb9942
Jonathan Alphabet Sutanto <jonathan.alphabet@jasuindoad.local>
2025-03-03 16:53:43 +0700

    frontend template

---
+++ frontend/pages/cashier/transaction/[id].vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <Header />
+
+    <div class="container mx-auto px-6 py-12">
+
+      <div class="mt-8">
+        <TableCashier :tableData="transactionsData"/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import TableCashier from "~/components/Cashier/TableCashier.vue";
+
+const transactionsData = [{
+  code: "9755142",
+  name: "haahhaha",
+  stock: 99,
+  price: 9999
+},{
+  code: "9755144",
+  name: "haahhaha",
+  stock: 99,
+  price: 9999
+},{
+  code: "9755145",
+  name: "haahhaha",
+  stock: 99,
+  price: 9999
+},{
+  code: "9755149",
+  name: "haahhaha",
+  stock: 99,
+  price: 9999
+}]
+</script>
---
+++ frontend/pages/cashier/transaction/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <Header />
+
+    <div class="container mx-auto px-6 py-12">
+
+      <div class="mt-8">
+        <TransactionTable :tableData="transactionsData"/>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+
+import TransactionTable from "~/components/Transaction/TransactionTable.vue";
+
+const transactionsData = ref([{
+  code: "9755142",
+  date: "27 Aug 2025",
+  user: "haahhaha",
+  total: 9999
+},
+  {
+    code: "9755142",
+    date: "27 Aug 2025",
+    user: "haahhaha",
+    total: 9251
+  },
+  {
+    code: "9755142",
+    date: "27 Aug 2025",
+    user: "haahhaha",
+    total: 111
+  },
+  {
+    code: "9755142",
+    date: "27 Aug 2025",
+    user: "haahhaha",
+    total: 933
+  },
+])
+</script>
